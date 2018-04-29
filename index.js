8d02156708df5a0ab7d0f17f4b852b259ed3da68
Jake Wilson <jakewilson780@gmail.com>
2018-04-29 00:55:18 -0400

    Added mongoose schema

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 import express from 'express';
+import mongoose from 'mongoose';
 import routes from './src/routes/route';
 const app = express();
 const PORT = 3000;
 
-routes(app);
+//mongoose connection
+mongoose.connect('mongodb://localhost/project2'); //database connect
+
+//bodyparser
+express.urlencoded({extended: true}); //parses incoming requests with urlencoded payloads
+express.json(); //parses incoming requests with JSON payloads
+
+routes(app); //allows for use of routes created earlier
 
 app.get('/', (req, res) =>
     res.send(`Node and express server is running on port ${PORT}`)
--- src/routes/route.js
+++ src/routes/route.js
@@ -2,9 +2,14 @@ const routes = (app) => {
 
     //Contact Route
     app.route('/contact')
-    .get((req,res) => 
+    .get((req,res,next) => { 
+        //middleware
+        console.log(`Request from:${req.originalUrl}`)
+        console.log(`Request from:${req.method}`) 
+        next(); //allows the next function to happen
+    }, (req,res,next) => {
         res.send('GET Request sucessfull')
-    )
+    })
     .post((req,res) => 
         res.send('Post Reqest sucessfull')
     );
