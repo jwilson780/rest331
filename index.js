4eff522e3014861a5c476b3edcde9a3fb4db322e
Jake Wilson <jakewilson780@gmail.com>
2018-04-29 01:30:05 -0400

    prettied formatting

--- index.js
+++ index.js
@@ -1,22 +1,20 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import routes from './src/routes/route';
+import express from "express";
+import mongoose from "mongoose";
+import routes from "./src/routes/route";
 const app = express();
 const PORT = 3000;
 
 //mongoose connection
-mongoose.connect('mongodb://localhost/project2'); //database connect
+mongoose.connect("mongodb://localhost/project2"); //database connect
 
 //bodyparser
-express.urlencoded({extended: true}); //parses incoming requests with urlencoded payloads
+express.urlencoded({ extended: true }); //parses incoming requests with urlencoded payloads
 express.json(); //parses incoming requests with JSON payloads
 
 routes(app); //allows for use of routes created earlier
 
-app.get('/', (req, res) =>
-    res.send(`Node and express server is running on port ${PORT}`)
+app.get("/", (req, res) =>
+  res.send(`Node and express server is running on port ${PORT}`)
 );
 
-app.listen(PORT, () =>
-    console.log(`your server is running on port ${PORT}`)
-);
+app.listen(PORT, () => console.log(`your server is running on port ${PORT}`));
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,28 +1,26 @@
-const routes = (app) => {
-
-    //Contact Route
-    app.route('/contact')
-    .get((req,res,next) => { 
+const routes = app => {
+  //Contact Route
+  app
+    .route("/contact")
+    .get(
+      (req, res, next) => {
         //middleware
-        console.log(`Request from:${req.originalUrl}`)
-        console.log(`Request from:${req.method}`) 
+        console.log(`Request from:${req.originalUrl}`);
+        console.log(`Request from:${req.method}`);
         next(); //allows the next function to happen
-    }, (req,res,next) => {
-        res.send('GET Request sucessfull')
-    })
-    .post((req,res) => 
-        res.send('Post Reqest sucessfull')
-    );
-
-    //Contact ID routes
-
-    app.route('/contact/:contactId')
-    .put((req,res) =>
-        res.send('PUT request successfull')
+      },
+      (req, res, next) => {
+        res.send("GET Request sucessfull");
+      }
     )
-    .delete((req,res) =>
-        res.send('DELETE request sucessfull')
-    );
-}
+    .post((req, res) => res.send("Post Reqest sucessfull"));
+
+  //Contact ID routes
+
+  app
+    .route("/contact/:contactId")
+    .put((req, res) => res.send("PUT request successfull"))
+    .delete((req, res) => res.send("DELETE request sucessfull"));
+};
 
 export default routes;
